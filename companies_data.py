22ae5b0b7326b96562bb746a1d384a7430f94775
Mike Fanor <45322219+1meekay@users.noreply.github.com>
2019-03-10 12:20:45 -0400

    Add files via upload

--- companies_data.py
+++ companies_data.py
@@ -30,8 +30,10 @@ dataframe = pd.DataFrame(data=data, index=ids, columns=cols)
 
 # ======================================================================================================================
 
-# Business Class :: Team NoName
-# Quantifying the health of a small business
+"""
+Business Class :: Team Quantify
+Quantifying the health of a small business
+"""
 
 class Business:
 
@@ -127,9 +129,9 @@ class Business:
         return td
 
     # Debt ratios 0.4 and lower signal healthy debt
-    def debt_ratio(self):
-        dr = (self.current_liabilities + self.long_term_liabilities) / (self.current_assets + self.cash_balance)
-        return dr
+    def debt_asset_ratio(self):
+        dar = (self.current_liabilities + self.long_term_liabilities) / (self.current_assets + self.cash_balance)
+        return dar
 
     # Break-even expenses should be less than total revenue
     # Aggressive expansion may cause losses, but doesn't signal bad health
@@ -147,38 +149,32 @@ class Business:
         ih = (self.dead_inventory / self.total_inventory) * 100.0
         return ih
 
-    # def outlook(self):
-        proj_rev = self.avg_ticket_size * self.exp_transactions
-        return "{:.{}f}%".format(proj_rev, 2)
-
 # ======================================================================================================================
 
 def recurse(name, location, industry, total_revenue, prior_revenue, cogs, op_expenses, cash_balance, current_assets,
             current_liabilities, long_term_liabilities, owner_sat, employee_sat, new_customers, start_customers,
             end_customers, rewards_program, total_inventory, dead_inventory, expansion):
-    try:
-        name = name
-        location = location
-        industry = industry
-        total_revenue = float(total_revenue)
-        prior_revenue = float(prior_revenue)
-        cogs = float(cogs)
-        op_expenses = float(op_expenses)
-        cash_balance = float(cash_balance)
-        current_assets = float(current_assets)
-        current_liabilities = float(current_liabilities)
-        long_term_liabilities = float(long_term_liabilities)
-        owner_satisfaction = int(owner_sat)
-        employee_satisfaction = int(employee_sat)
-        new_customers = int(new_customers)
-        start_customers = int(start_customers)
-        end_customers = int(end_customers)
-        rewards_program = rewards_program
-        total_inventory = float(total_inventory)
-        dead_inventory = float(dead_inventory)
-        expansion = expansion
-    except ValueError:
-        print('An exception occurred')
+
+    name = name
+    location = location
+    industry = industry
+    total_revenue = float(total_revenue)
+    prior_revenue = float(prior_revenue)
+    cogs = float(cogs)
+    op_expenses = float(op_expenses)
+    cash_balance = float(cash_balance)
+    current_assets = float(current_assets)
+    current_liabilities = float(current_liabilities)
+    long_term_liabilities = float(long_term_liabilities)
+    owner_satisfaction = int(owner_sat)
+    employee_satisfaction = int(employee_sat)
+    new_customers = int(new_customers)
+    start_customers = int(start_customers)
+    end_customers = int(end_customers)
+    rewards_program = rewards_program
+    total_inventory = float(total_inventory)
+    dead_inventory = float(dead_inventory)
+    expansion = expansion
 
     business_data = {}
     business_dict = {}
@@ -216,14 +212,14 @@ def recurse(name, location, industry, total_revenue, prior_revenue, cogs, op_exp
         liq_str = "{:.{}f}".format(liq, 1)
         print("Liquidity: ", liq_str)
 
-        debt_service_coverage = npl / total_revenue
+        debt_service_coverage = npl / sample_business.current_liabilities
         td = debt_service_coverage
-        td_str = "${:.{}f}".format(td, 2)
-        print("Total debt: ", td_str)
+        td_str = "{:.{}f}".format(td, 2)
+        print("Debt service coverage: ", td_str)
 
-        dr = sample_business.debt_ratio()
-        dr_str = "{:.{}f}%".format(dr, 1)
-        print("Debit-to-asset ratio: ", dr_str)
+        dar = sample_business.debt_asset_ratio()
+        dar_str = "{:.{}f}".format(dar, 1)
+        print("Debit-to-asset ratio: ", dar_str)
 
         # be = sample_business.break_even()
         # print('Monthly break-even amount: ', be)
@@ -243,16 +239,16 @@ def recurse(name, location, industry, total_revenue, prior_revenue, cogs, op_exp
         print()
 
         # Adding business data to dictionary
-        business_data['gpm'] = round(gpm, 2)
-        business_data['npl'] = round(npl, 2)
-        business_data['liq'] = round(liq, 2)
-        business_data['td'] = round(td, 2)
-        business_data['dr'] = round(dr, 2)
+        business_data['gross profit margin'] = round(gpm, 2)
+        business_data['net profit/loss'] = round(npl, 2)
+        business_data['liquidity'] = round(liq, 2)
+        business_data['debt service coverage'] = round(td, 2)
+        business_data['debt-to-asset ratio'] = round(dar, 2)
         # business_data['be'] = be
-        business_data['crr'] = round(crr, 2)
-        business_data['sat'] = round(sat, 2)
-        business_data['i_health'] = round(i_health, 2)
-        business_data['growth'] = round(growth, 2)
+        business_data['customer retention rate'] = round(crr, 2)
+        business_data['employee/owner satisfaction'] = round(sat, 2)
+        business_data['dead inventory'] = round(i_health, 2)
+        business_data['month-over-month growth'] = round(growth, 2)
 
         # Creating dictionary of (possible) multiple businesses
         business_dict[name] = business_data
@@ -262,156 +258,161 @@ def recurse(name, location, industry, total_revenue, prior_revenue, cogs, op_exp
 
             # Metric 1: Gross profit margin
             def gpm_health(business_data):
-                if business_data['gpm'] >= 60:
+                if business_data['gross profit margin'] >= 60:
                     return 11
-                elif 50 <= business_data['gpm'] < 60:
+                elif 50 <= business_data['gross profit margin'] < 60:
                     return 9
-                elif 30 <= business_data['gpm'] < 50:
+                elif 30 <= business_data['gross profit margin'] < 50:
                     return 7
-                elif 20 <= business_data['gpm'] < 30:
+                elif 20 <= business_data['gross profit margin'] < 30:
                     return 5
-                elif 10 <= business_data['gpm'] < 20:
+                elif 10 <= business_data['gross profit margin'] < 20:
                     return 3
-                elif 5 <= business_data['gpm'] < 10:
+                elif 5 <= business_data['gross profit margin'] < 10:
                     return 1
 
             # Metric 2: Net profit/loss
             def npl_health(business_data, expansion):
                 if expansion.lower() == 'y':
-                    if business_data['npl'] > 0:
+                    if business_data['net profit/loss'] > 0:
                         return 11
-                    return 8
+                    return 9
                 elif expansion.lower() == 'n':
-                    if business_data['npl'] >= 5000:
+                    if business_data['net profit/loss'] >= 5000:
                         return 11
-                    elif 5000 > business_data['npl'] >= 2000:
+                    elif 5000 > business_data['net profit/loss'] >= 2000:
+                        return 10
+                    elif 2000 > business_data['net profit/loss'] >= 1000:
                         return 9
-                    elif 2000 > business_data['npl'] >= 1000:
+                    elif 1000 > business_data['net profit/loss'] >= 0:
+                        return 8
+                    elif -1000 < business_data['net profit/loss'] < 0:
                         return 7
-                    elif 1000 > business_data['npl'] >= 0:
+                    elif -2000 < business_data['net profit/loss'] <= -1000:
+                        return 6
+                    elif -5000 < business_data['net profit/loss'] <= -2000:
                         return 5
-                    elif -1000 < business_data['npl'] < 0:
-                        return 3
-                    elif -2000 < business_data['npl'] <= -1000:
-                        return 2
-                    elif -5000 < business_data['npl'] <= -2000:
-                        return 1
-                    return 0
+                    return 4
 
             # Metric 3: Liquidity
+            # > 1.0 is considered good
             def liq_health(business_data):
-                if business_data['liq'] > 1.8:
+                if business_data['liquidity'] > 1.8:
                     return 11
-                elif 1.2 < business_data['liq'] <= 1.8:
+                elif 1.2 < business_data['liquidity'] <= 1.8:
+                    return 10
+                elif 0.8 < business_data['liquidity'] <= 1.2:
                     return 9
-                elif 0.8 < business_data['liq'] <= 1.2:
-                    return 7
-                elif 0.4 < business_data['liq'] <= 0.8:
-                    return 5
-                return 3
+                elif 0.4 < business_data['liquidity'] <= 0.8:
+                    return 8
+                return 7
 
             # Metric 4: Debt service coverage ratio (banks use this figure to determine loan qualifications)
             # > 1.25 is okay
             def td_health(business_data):
-                if business_data['td'] > 2:
+                if business_data['debt service coverage'] > 2:
                     return 11
-                elif 1.5 < business_data['td'] <= 2:
+                elif 1.5 < business_data['debt service coverage'] <= 2:
+                    return 10
+                elif 1.25 < business_data['debt service coverage'] <= 1.5:
                     return 9
-                elif 1.25 < business_data['td'] <= 1.5:
+                elif 1.0 < business_data['debt service coverage'] <= 1.25:
+                    return 8
+                elif 0.8 < business_data['debt service coverage'] <= 1.0:
                     return 7
-                elif 1.0 < business_data['td'] <= 1.25:
-                    return 5
-                elif 0.8 < business_data['td'] <= 1.0:
-                    return 3
-                return 0
+                return 6
 
             # Metric 5: Debt-to-asset ratio
-            def dr_health(business_data):
-                if business_data['dr'] < 0.4:
+            # The higher the ratio, the greater the risk
+            def dar_health(business_data):
+                if business_data['debt-to-asset ratio'] < 0.4:
                     return 11
-                elif 0.6 > business_data['dr'] >= 0.4:
+                elif 0.5 > business_data['debt-to-asset ratio'] >= 0.4:
+                    return 10
+                elif 0.6 > business_data['debt-to-asset ratio'] >= 0.5:
+                    return 9
+                elif 0.7 > business_data['debt-to-asset ratio'] >= 0.6:
+                    return 8
+                elif 0.8 > business_data['debt-to-asset ratio'] >= 0.7:
                     return 7
-                elif 0.8 > business_data['dr'] >= 0.6:
-                    return 3
-                return 0
+                return 6
 
             # Metric 6: Customer retention rate
+            # 90% and above is great
             def crr_health(business_data):
-                if business_data['crr'] >= 90:
-                    return 11
-                elif 80 <= business_data['crr'] < 90:
-                    return 9
-                elif 70 <= business_data['crr'] < 80:
-                    return 7
-                elif 60 <= business_data['crr'] < 70:
-                    return 4
-                return 0
+                points = 1
+                if sample_business.rewards_program.lower() == 'y':
+                    if business_data['customer retention rate'] >= 90:
+                        return 11 + points
+                    elif 80 <= business_data['customer retention rate'] < 90:
+                        return 10 + points
+                    elif 70 <= business_data['customer retention rate'] < 80:
+                        return 9 + points
+                    elif 60 <= business_data['customer retention rate'] < 70:
+                        return 8 + points
+                    return 6 + points
+                elif sample_business.rewards_program.lower() == 'n':
+                    if business_data['customer retention rate'] >= 90:
+                        return 10
+                    elif 80 <= business_data['customer retention rate'] < 90:
+                        return 9
+                    elif 70 <= business_data['customer retention rate'] < 80:
+                        return 8
+                    elif 60 <= business_data['customer retention rate'] < 70:
+                        return 7
+                    return 5
 
             # Metric 7: Owner & Employee satisfaction
             def empl_health(business_data):
-                return business_data['sat']
+                return business_data['employee/owner satisfaction']
 
             # Metric 8: Inventory health
+            # The lower the dead inventory percentage, the better
             def inventory_health(business_data):
-                points = 1
-                if sample_business.rewards_program.lower() == 'y':
-                    if business_data['i_health'] <= 0.05:
-                        return 11 + points
-                    elif 0.05 < business_data['i_health'] <= 0.10:
-                        return 9 + points
-                    elif 0.10 < business_data['i_health'] <= 0.15:
-                        return 7 + points
-                    elif 0.15 < business_data['i_health'] <= 0.2:
-                        return 5 + points
-                    elif 0.2 < business_data['i_health'] <= 0.25:
-                        return 3 + points
-                    elif 0.25 < business_data['i_health'] <= 0.3:
-                        return 1 + points
-                    return 0
-                else:
-                    if business_data['i_health'] <= 0.05:
-                        return 10
-                    elif 0.05 < business_data['i_health'] <= 0.10:
-                        return 8
-                    elif 0.10 < business_data['i_health'] <= 0.15:
-                        return 6
-                    elif 0.15 < business_data['i_health'] <= 0.2:
-                        return 4
-                    elif 0.2 < business_data['i_health'] <= 0.25:
-                        return 2
-                    elif 0.25 < business_data['i_health'] <= 0.3:
-                        return 1
-                    return 0
+                if business_data['dead inventory'] <= 5:
+                    return 11
+                elif 5 < business_data['dead inventory'] <= 10:
+                    return 10
+                elif 10 < business_data['dead inventory'] <= 15:
+                    return 9
+                elif 15 < business_data['dead inventory'] <= 20:
+                    return 8
+                elif 20 < business_data['dead inventory'] <= 25:
+                    return 7
+                elif 25 < business_data['dead inventory'] <= 30:
+                    return 6
+                return 5
 
-            # Metric 9: Growth
+            # Metric 9:  Growth
+            # You're either growing or your dying
             def growth_health(business_data):
-                if sample_business.prior_revenue > sample_business.total_revenue:
+                if sample_business.prior_revenue < sample_business.total_revenue:
                     return 11
                 else:
-                    return 5
+                    return 6
 
-            # Health computation (aggregate)
+            # Health computation (in aggregate)
             def health_compilation():
                 metric_1 = gpm_health(business_data)
                 metric_2 = npl_health(business_data, expansion)
                 metric_3 = liq_health(business_data)
                 metric_4 = td_health(business_data)
-                metric_5 = dr_health(business_data)
+                metric_5 = dar_health(business_data)
                 metric_6 = crr_health(business_data)
                 metric_7 = empl_health(business_data)
                 metric_8 = inventory_health(business_data)
                 metric_9 = growth_health(business_data)
 
                 # Adding metrics to dictionary
-                business_metrics['gpm'] = metric_1
-                business_metrics['npl'] = metric_2
-                business_metrics['liq'] = metric_3
-                business_metrics['td'] = metric_4
-                business_metrics['dr'] = metric_5
-                business_metrics['crr'] = metric_6
-                business_metrics['sat'] = metric_7
-                business_metrics['i_health'] = metric_8
-                business_metrics['growth'] = metric_9
+                business_metrics['gross profit margin'] = metric_1
+                business_metrics['net profit/loss'] = metric_2
+                business_metrics['liquidity'] = metric_3
+                business_metrics['debt service coverage'] = metric_4
+                business_metrics['debt-to-asset ratio'] = metric_5
+                business_metrics['customer retention rate'] = metric_6
+                business_metrics['employee/owner satisfaction'] = metric_7
+                business_metrics['dead inventory'] = metric_8
+                business_metrics['month-over-month growth'] = metric_9
 
                 business_dict[name + '_metrics'] = business_metrics
 
@@ -422,6 +423,30 @@ def recurse(name, location, industry, total_revenue, prior_revenue, cogs, op_exp
 
         health_checkup(business_data)
 
+        def review():
+            if business_final_grade[name] >= 90:
+                print('Grade A :: Excellent! You are on the fast-track for growth and sustainability.')
+            elif 90 > business_final_grade[name] >= 80:
+                print('Grade B :: Good Job! Your data points look good. As you expand, try to keep'
+                      ' costs the same as you expand and scale your business.')
+            elif 80 > business_final_grade[name] >= 70:
+                print('Grade C :: There is more work to accomplish. Keep an eye on debt and dead inventory. '
+                      'Ensure the company is taking care of the employees and customers.')
+            elif 70 > business_final_grade[name] >= 60:
+                print('Grade D :: Whoa, buddy! Debt overload. Profit margins are too low and your customers need to'
+                      'know they are loved.')
+            else:
+                print(
+                    'Grade F :: No bueno! Give extra attention to your suppliers and bring your costs down. Watch out '
+                    'for payroll expenses, over-aggressive expansion, and large short-term debt obligations. Most '
+                    'importantly, take extra special care of your employees and customers. They are the life blood of '
+                    'your business.')
+
+        review()
+        print()
+
+    # Running the program
+    # if __name__ == '__main__':
     add_business()
 
     print('==============================================')
